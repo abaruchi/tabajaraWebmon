b94e7784216bff784c494971b9fe5485438f5a9e
Artur Baruchi <artur.baruchi@gmail.com>
2021-04-05 19:02:06 -0300

    Implements methods for basic and regex probe for HTTP + Builder class

--- webmon/webmon/Probe.py
+++ webmon/webmon/Probe.py
@@ -20,25 +20,56 @@ class Probe(ABC):
 
 class HTTPProbe(Probe):
 
-    def basic_probe(self):
-        print("Basic Probe - ")
+    def basic_probe(self, req_timeout: int = 10) -> dict:
+        """
+        This method runs the basic probe and stores the result in a dictionary.
 
-    def regex_probe(self):
-        pass
+        :return: A dictionary with basic probe information
+        """
+
+        try:
+            http_probe = requests.get(self.host, timeout=req_timeout)
+            http_probe_response = {
+                'return_code': http_probe.status_code,
+                'response_time_sec': http_probe.elapsed.total_seconds(),
+                'page_content': http_probe.text
+            }
+        except TimeoutError:
+            http_probe_response = {
+                'return_code': 0,
+                'response_time_sec': float('Inf'),
+                'page_content': ''
+            }
+
+        return http_probe_response
+
+    def regex_probe(self, page_content: str, regex: str) -> bool:
+        """
+        This method runs a match regex into the page content. If the regex is
+        found it returns True, otherwise it returns False.
+
+        :param page_content: The page content to search for a regex match
+        :param regex: The regex to match
+        :return: True if there is a match, False otherwise
+        """
+
+        pattern = re.compile(regex)
+        if pattern.findall(page_content):
+            return True
+        return False
 
 
-def create_monitor(app_protocol: str = 'HTTP', **kwargs):
+class CreateProbe:
+    """
+    This class is used to build Monitor Objects. When adding new Appl Protocols
+    another entry should be added to this class. Since the absence of a protocol
+    implementation is an unrecoverable error a NotImplementedError is raised.
     """
 
-    """
+    def get_probe(self, hostname: str, probe_details: dict,
+                  probe_proto: str = 'HTTP'):
 
-    probe = {
-        'HTTP': HTTPProbe
-    }
-
-    try:
-        probe_obj = probe[app_protocol](**kwargs)
-        return probe_obj
-    except KeyError as e:
-        print("Protocol {} Probe not Implemented".format(app_protocol))
-        print("Error: {}".format(e))
+        if probe_proto == 'HTTP':
+            return HTTPProbe(hostname, probe_details)
+        else:
+            raise NotImplementedError
