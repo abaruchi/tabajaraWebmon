1ab12d83db24c3434a7b69f5d15f2f07414e6a9e
Artur Baruchi <artur.baruchi@gmail.com>
2021-04-05 18:59:18 -0300

    Adds a temporary code to runner.py to perform minor test

--- webmon/runner.py
+++ webmon/runner.py
@@ -1,7 +1,18 @@
 from webmon import Config
+from webmon import Probe
 
 c = Config.user_config
-print(c.list_hosts())
-print(c.get_mon_details('https://aiven.io/'))
+probe_creator = Probe.CreateProbe()
+
+for host in c.list_hosts():
+
+    print("Probe: {}".format(host))
+    probe_details = c.get_mon_details(host)
+    http_probe = probe_creator.get_probe(host, probe_details)
+    probe_result = http_probe.basic_probe()
+
+    if len(probe_details['REGEX_MON']) > 0:
+        for regex in probe_details['REGEX_MON']:
+            print(http_probe.regex_probe(probe_result['page_content'], regex))
 
 print("Hi there.. Im TABAJARA WebMon")
