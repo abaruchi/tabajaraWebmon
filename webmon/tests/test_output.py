6f8142efb4f1edc9180d70753f70822c24a29844
Artur Baruchi <artur.baruchi@gmail.com>
2021-04-06 20:28:58 -0300

    Adds unit test to output class

---
+++ webmon/tests/test_output.py
@@ -0,0 +1,26 @@
+import unittest
+
+from webmon.webmon import Config, Output
+
+
+class TestProbe(unittest.TestCase):
+
+    def setUp(self) -> None:
+
+        self.conf_missing_fields = {
+                "AUTH":
+                {
+                    "ssl_cafile": "ca.pem",
+                    "ssl_certfile": "service.cert"
+                },
+            "CONNECTION":
+                {
+                    "host": "",
+                    "port": ""
+                }
+        }
+
+    def test_invalid_aiven_conn(self):
+
+        with self.assertRaises(ConnectionAbortedError):
+            Output.ToKafka(self.conf_missing_fields)
